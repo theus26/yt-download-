73b44893634a897d39e0dae2a1e18ab82418de12
Matheus <theuslipe26@gmail.com>
2024-09-17 22:26:11 -0300

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="tw-text-red-600">Hello world!</h1>
+  </main>
+</template>
